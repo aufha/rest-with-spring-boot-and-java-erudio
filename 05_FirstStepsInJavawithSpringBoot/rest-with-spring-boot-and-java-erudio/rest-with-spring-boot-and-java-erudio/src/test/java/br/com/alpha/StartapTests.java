156b25efbe6b598eaecbcbf994bf82f1e0ceef1a
alpha.series <aufa9234@gmail.com>
2022-09-19 18:17:20 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/alpha/StartapTests.java
@@ -0,0 +1,13 @@
+package br.com.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartapTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
